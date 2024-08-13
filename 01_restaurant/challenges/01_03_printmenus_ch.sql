efc2494be12df1d602a61a4e8dfd301e7aad9b83
mtnleo <mtnleonardi@gmail.com>
2024-08-13 19:26:46 -0300

    Challenges 01-01 to 01-05 completed

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,14 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+SELECT Name, Price, Description, Type
+FROM Dishes
+ORDER BY Price DESC;
+
+SELECT Name, Price, Description, Type
+FROM Dishes
+WHERE Type LIKE ('Appetizer') OR Type LIKE ('Beverage');
+
+SELECT Name, Price, Description, Type
+FROM Dishes
+WHERE Type NOT LIKE ('Beverage');
--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,14 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+UPDATE Customers
+SET Address = '74 Pine St.', City = 'New York', State = 'NY'
+WHERE Customers.FirstName = ('Taylor') AND Customers.LastName = ('Jenkins');
+
+SELECT *
+FROM CUSTOMERS 
+WHERE Customers.FirstName = ('Taylor') AND Customers.LastName = ('Jenkins');
+
+UPDATE Customers
+SET Address = '968 Bartillon Park', City = 'New York', State = 'NY'
+WHERE CustomerID = 4; -- Quick fix lol
