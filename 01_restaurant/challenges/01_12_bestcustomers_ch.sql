d1fbfe31fee91392602d84e66f34278f0894aa3f
mtnleo <mtnleonardi@gmail.com>
2024-08-14 18:01:09 -0300

    Restaurant excercises completed

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,10 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+SELECT COUNT(o.OrderID) AS number_of_orders, c.FirstName, c.LastName, c.Email
+FROM Customers AS c 
+JOIN Orders AS o 
+ON o.CustomerID = c.CustomerID 
+GROUP BY o.CustomerID
+ORDER BY number_of_orders DESC
+LIMIT 15;
